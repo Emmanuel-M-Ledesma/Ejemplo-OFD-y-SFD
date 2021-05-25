d2fc6e6daeeda9ac3f1f311b7cca1a744c74d525
Emmanuel-M-Ledesma <65962173+Emmanuel-M-Ledesma@users.noreply.github.com>
2021-05-25 19:48:08 -0300

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btAbrir = new System.Windows.Forms.Button();
+            this.lblRuta = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btGuardar = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.txtText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btAbrir
+            // 
+            this.btAbrir.Location = new System.Drawing.Point(210, 38);
+            this.btAbrir.Name = "btAbrir";
+            this.btAbrir.Size = new System.Drawing.Size(75, 23);
+            this.btAbrir.TabIndex = 0;
+            this.btAbrir.Text = "Abrir";
+            this.btAbrir.UseVisualStyleBackColor = true;
+            this.btAbrir.Click += new System.EventHandler(this.btAbrir_Click);
+            // 
+            // lblRuta
+            // 
+            this.lblRuta.AutoSize = true;
+            this.lblRuta.Location = new System.Drawing.Point(55, 81);
+            this.lblRuta.Name = "lblRuta";
+            this.lblRuta.Size = new System.Drawing.Size(86, 13);
+            this.lblRuta.TabIndex = 1;
+            this.lblRuta.Text = "Ruta del Archivo";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btGuardar
+            // 
+            this.btGuardar.Location = new System.Drawing.Point(57, 38);
+            this.btGuardar.Name = "btGuardar";
+            this.btGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btGuardar.TabIndex = 2;
+            this.btGuardar.Text = "Guardar";
+            this.btGuardar.UseVisualStyleBackColor = true;
+            this.btGuardar.Click += new System.EventHandler(this.btGuardar_Click);
+            // 
+            // txtText
+            // 
+            this.txtText.Location = new System.Drawing.Point(57, 121);
+            this.txtText.Name = "txtText";
+            this.txtText.Size = new System.Drawing.Size(228, 20);
+            this.txtText.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 239);
+            this.Controls.Add(this.txtText);
+            this.Controls.Add(this.btGuardar);
+            this.Controls.Add(this.lblRuta);
+            this.Controls.Add(this.btAbrir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btAbrir;
+        private System.Windows.Forms.Label lblRuta;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btGuardar;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TextBox txtText;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btAbrir_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.InitialDirectory = "C://Users/Emale/Desktop/Itsc- 2 año";
+            openFileDialog1.Filter = "Archivos de texto(*.txt)|*.txt";
+            //openFileDialog1.Filter = "Archivos JPEG(*.jpeg)|*.jpeg";
+            openFileDialog1.AddExtension = true;
+            openFileDialog1.FileName = "";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                lblRuta.Text = openFileDialog1.FileName;
+                txtText.Text = openFileDialog1.FileName;    
+            }
+        }
+
+        private void btGuardar_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "Archivos de texto(*.txt)|*.txt";
+            saveFileDialog1.FileName = "";            
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)    
+            {
+
+            }
+        }
+    }
+}
